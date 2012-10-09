61e1eb0f3aa5f07f7c329b24a3e867791a65eb42
Snadde <thituson@gmail.com>
2012-10-09 16:00:59 +0200

    Add: beans for Customerhandling

---
+++ monkeyBusiness/src/main/java/AddAddressBackingBean.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author thituson
+ */
+import java.io.Serializable;
+import javax.enterprise.context.*;
+import javax.inject.Named;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Named
+@RequestScoped
+public class AddAddressBackingBean implements Serializable{
+    
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z]*")
+    @Size(min = 1, max = 10, message = "Must use 1-10 chars")
+    private String street;
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z]*")
+    @Size(min = 1, max = 10, message = "Must use 1-10 chars")
+    private String town;
+    @NotNull
+    @Min(value = 1, message = "To small")
+    @Max(value = 10000, message = "To big")
+    private int nbr;
+    
+    
+    public AddAddressBackingBean(){}
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getNbr() {
+        return nbr;
+    }
+
+    public void setNbr(int nbr) {
+        this.nbr = nbr;
+    }
+    
+
+}
---
+++ monkeyBusiness/src/main/java/AddCustomerControlBean.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author thituson
+ */
+import core.Address;
+import core.Customer;
+import core.Product;
+import java.io.Serializable;
+import javax.enterprise.context.*;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class AddCustomerControlBean implements Serializable{
+    @Inject
+    private AddAddressBackingBean addAddressBackingBean;
+    @Inject
+    private AddCustomerBackingBean addCustomerBackingBean;
+    @Inject
+    private CustomersBackingBean customersBackingBean;
+    
+    public AddCustomerControlBean(){}
+        
+    public String action() {
+        String town = addAddressBackingBean.getTown();
+        String street = addAddressBackingBean.getStreet();
+        int nbr = addAddressBackingBean.getNbr();
+        Address address = new Address(street, nbr, town);
+        String fname = addCustomerBackingBean.getFname();
+        String lname = addCustomerBackingBean.getLname();
+        String email = addCustomerBackingBean.getEmail();
+        Customer customer = new Customer(address, fname, lname, email);
+        customersBackingBean.add(customer);
+        return "adminProducts?faces-redirect=true";  // Where to go, navigate 
+    }
+    
+    
+
+
+    
+    
+    
+}
