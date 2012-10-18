0f86f046c43bc1697c4ede60a9fa5024182f52fb
Snadde <thituson@gmail.com>
2012-10-18 12:28:16 +0200

    Add: Javadoc comments added

--- monkeyBusiness/src/main/java/controlbeans/AddCustomerControlBean.java
+++ monkeyBusiness/src/main/java/controlbeans/AddCustomerControlBean.java
@@ -1,13 +1,14 @@
 package controlbeans;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
+ * This class acts like the controller and is conversationScoped. It starts a
+ * conversation to get all the attributes from the backing bean (view par). It
+ * checks so the username isn´t in use before it ends the conversation and sends
+ * an add request to the customer registry bean. If the username is in use it
+ * sends back a message and the attributes is still in the form because the
+ * conversation is not ended.
  *
- * @author thituson
+ * @author Patrik Thituson
  */
 import backingbeans.AddAddressBackingBean;
 import backingbeans.AddCustomerBackingBean;
@@ -27,8 +28,9 @@ import modelbeans.CustomerRegistryBean;
 
 @Named
 @ConversationScoped
-public class AddCustomerControlBean implements Serializable{
-    @Inject // Handled by system, don't need to create class.
+public class AddCustomerControlBean implements Serializable {
+
+    @Inject 
     private Conversation conv;
     @Inject
     private AddAddressBackingBean addAddressBackingBean;
@@ -36,53 +38,49 @@ public class AddCustomerControlBean implements Serializable{
     private AddCustomerBackingBean addCustomerBackingBean;
     @Inject
     private CustomerRegistryBean customerRegistryBean;
-    
-    public AddCustomerControlBean(){}
-        
+
+    public AddCustomerControlBean() {
+    }
+
+    /**
+     * Starts a conversation and gets the username to check that it is not
+     * taken, if it is the method returns null and sets a message. The form is
+     * still filled in except the password. If the username is available it gets
+     * all attributes form the backing bean and creates a new customer which it
+     * sends to the custom registry bean to add it and redirect the user to shopProducts site.
+     *
+     * @return the page which to be redirected or null if the username already
+     * exist, which means that it stays on the same page with the form filled
+     */
     public String action() {
         if (conv.isTransient()) {
             conv.begin();
-             Logger.getAnonymousLogger().log(Level.INFO, "CONVERSATION BEGINS: Got pnumb {0}");
-        }else{
-            
+            Logger.getAnonymousLogger().log(Level.INFO, "CONVERSATION BEGINS: Got pnumb {0}");
+        } else {
+        }
+
+        String userName = addCustomerBackingBean.getUserName();
+        if (customerRegistryBean.isUserNameTaken(userName)) {
+            FacesContext.getCurrentInstance().addMessage("regForm:username", new FacesMessage("Username is already in use"));
+            return null;
+
         }
         String town = addAddressBackingBean.getTown();
         String street = addAddressBackingBean.getStreet();
         int nbr = addAddressBackingBean.getNbr();
         Address address = new Address(street, nbr, town);
-        addCustomerBackingBean.setAddress(address);
         String fname = addCustomerBackingBean.getFname();
         String lname = addCustomerBackingBean.getLname();
         String email = addCustomerBackingBean.getEmail();
-        String userName =addCustomerBackingBean.getUserName();
         String password = addCustomerBackingBean.getPassword();
-        
-        if(customerRegistryBean.isUserNameTaken(userName))
-        {
-            FacesContext.getCurrentInstance().addMessage("regForm:username", new FacesMessage("Username is already in use"));
-            
-       
-            
-            //FacesContext context = FacesContext.getCurrentInstance();
-           // context.addMessage( null, new FacesMessage( "The message to display in client" ));
-            return null;
-           // return "newCustomer?faces-redirect=true";
-        }
-        
-         if (!conv.isTransient()) {
+
+        if (!conv.isTransient()) {
             conv.end();
-             Logger.getAnonymousLogger().log(Level.INFO, "CONVERSATION ENDS");
+            Logger.getAnonymousLogger().log(Level.INFO, "CONVERSATION ENDS");
         }
-        
+
         Customer customer = new Customer(address, fname, lname, email, userName, password);
         customerRegistryBean.add(customer);
         return "shopProducts?faces-redirect=true";
     }
-    
-    
-
-
-    
-    
-    
 }
